c5400f5e171e65daebe28609e16a41b6369ba2ec
your_github_username <your_github_email>
2022-08-02 05:49:25 -0400

    add new recipes

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -4,4 +4,24 @@ class RecipesController < ApplicationController
         @recipes = Recipe.all
     end
 
+    def create
+        @recipe=Recipe.new(recipe_params)
+        @recipe.chef=Chef.first
+        @recipe.save
+        redirect_to recipe_path(@recipe)
+    end
+
+    def show
+        @recipe = Recipe.find(params[:id])
+    end
+
+    def new
+        @recipe = Recipe.new
+    end
+
+    private
+        def recipe_params
+            params.require(:recipe).permit(:name, :description)
+        end
+
 end
