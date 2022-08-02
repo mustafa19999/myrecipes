541d0923fb92dda08ef5988084a23d75b0722524
your_github_username <your_github_email>
2022-08-02 07:14:30 -0400

    edit and delete

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -5,11 +5,15 @@ class RecipesController < ApplicationController
     end
 
     def create
-        @recipe=Recipe.new(recipe_params)
-        @recipe.chef=Chef.first
-        @recipe.save
-        redirect_to recipe_path(@recipe)
-    end
+        @recipe = Recipe.new(recipe_params)
+        @recipe.chef = Chef.first
+        if @recipe.save
+          flash.alert = "Recipe was created successfully!"
+          redirect_to recipe_path(@recipe)
+        else
+          render 'new'
+        end
+      end
 
     def show
         @recipe = Recipe.find(params[:id])
@@ -17,6 +21,26 @@ class RecipesController < ApplicationController
 
     def new
         @recipe = Recipe.new
+    end
+
+    def edit
+        @recipe = Recipe.find(params[:id])
+    end
+
+    def update
+        @recipe = Recipe.find(params[:id])
+        if @recipe.update(recipe_params)
+        flash.alert = "Recipe was updated successfully!"
+        redirect_to recipe_path(@recipe)
+        else
+            render 'edit'
+        end
+    end
+
+    def destroy
+        Recipe.find(params[:id]).destroy
+        flash.alert = "Recipe deleted successfully"
+        redirect_to recipe_path
     end
 
     private
